1c13cbfe9b388c1c1003fe7940f8a72300995253
stephband <stephband@googlemail.com>
2021-07-06 02:18:15 +0200

    Imports routes into Literal

---
+++ data/location.js
@@ -0,0 +1,121 @@
+
+import Observer from '../modules/observer.js';
+import location from '../../dom/modules/location.js';
+
+const assign = Object.assign;
+
+export const defaults = {
+    search: '',
+    params: null,
+    hash:   '',
+    id:     '',
+    json:   'null',
+    state:  null
+};
+
+// Router scope
+
+const root  = assign(defaults);
+const scope = Observer(root);
+
+
+/** 
+routes()
+**/
+
+const names = [];
+
+function parseParam(string) {
+    var value;
+    return string === 'null' ? null :
+        string === 'true' ? true :
+        string === 'false' ? false :
+        // Number string to number
+        ((value = Number(string)) || value === 0) ? value :
+        // Comma delimited string to array
+        ((value = string.split(/\s*,\s*/)) && value.length > 1) ? value.map(parseParam) :
+        // Yer basic string
+        string ;
+}
+
+function fromEntries(entries) {
+    const object = {};
+    var key, value;
+
+    for([key, value] of entries) {
+        object[key] = parseParam(value);
+    }
+
+    return object;
+}
+
+function updateDataFromLocation(location, history, data) {
+    names.length = 0;
+
+    if (location.pathname !== data.pathname) {
+        data.pathname = location.pathname;
+        data.base  = '/';
+        data.path  = '';
+        data.route = location.pathname.slice(1);
+        names.push('route');
+    }
+
+    if (location.search !== data.search) {
+        data.search = location.search;
+        data.params = location.search ?
+            fromEntries(new URLSearchParams(location.search)) :
+            defaults.params ;
+        names.push('params');
+    }
+
+    if (location.hash !== data.hash) {
+        data.hash = location.hash;
+        data.id   = location.hash.replace(/^#/, '') || defaults.id;
+        names.push('id');
+    }
+
+    const json = JSON.stringify(history.state);
+    if (json !== data.json) {
+        data.json  = json;
+        data.state = history.state;
+        names.push('state');
+    }
+
+    return names;
+}
+
+
+/*
+const nostate = {
+    json:     'null',
+    state:    null
+};
+
+function updateData(location, data) {
+    return typeof location === 'string' ?
+        updateDataFromLocation({
+            pathname: location.replace(/(?:\?|#).*$/, ''),
+            search:   location.replace(/^[^?]* REMOVESPACE /, '').replace(/#.*$/, ''),
+            hash:     location.replace(/^[^#]* REMOVESPACE /, '')
+        }, nostate, data) :
+
+    // Or the global location object
+    location === window.location ?
+        updateDataFromLocation(location, window.history, data) :
+    
+    // Or another URL object
+        updateDataFromLocation(location, nostate, data) ;
+}
+*/
+
+location.on(function(location) {
+    const names = updateDataFromLocation(location, window.history, root);
+    var n = -1;
+    while (names[++n] !== undefined) {
+        Observer.notify(names[n], scope);
+    }
+    // Temp
+    //Observer.notify('location', scope);
+});
+
+export default scope;
--- library/routes.js
+++ library/routes.js
@@ -1,20 +1,122 @@
+
 import { register } from '../modules/library.js';
-import Observer from '../modules/observer.js';
-import routes from '../../dom/modules/routes.js';
-import location from '../../dom/modules/location.js';
+import Observer     from '../modules/observer.js';
+import { defaults } from '../data/location.js';
 
-register('routes', routes);
+const DEBUG = window.DEBUG && (window.DEBUG === true || window.DEBUG.includes('routes'));
 
+const assign = Object.assign;
 
-// Router scope
+/*
+function stop(route) {
+    //console.trace('stop', distributor);
+    const handlers = route.handlers;
+    var n = -1;
+    var handler;
 
-const scope = Observer({
-    location: window.location
+    while (handler = handlers[++n]) {
+        handler.stop && handler.stop();
+    }
+
+    // Throw away references to all handlers
+    handlers.length = 0;
+
+    // End of route group ***
+    if (DEBUG) { console.groupEnd(); }
+}
+*/
+
+function Route(base, path, route, captures) {
+    this.base       = base;
+    this.path       = path;
+    this.route      = route;
+
+    var n = -1;
+    while(captures[++n] !== undefined) {
+        this['$' + n] = captures[n];
+    }
+}
+
+assign(Route.prototype, {
+    params:     defaults.params,
+    identifier: defaults.identifier,
+    state:      defaults.state
 });
 
-export default scope;
+function router(patterns, keys, regexps, location, route) {
+    const base   = location.base + location.path;
+    const string = location.route;
 
-location.on((location) => {
-    Observer.notify('location', scope)
+    // Loop through regexes until a match is captured
+    var regexp, captures, n = -1;
+
+    while(
+        (regexp = regexps[++n]) && 
+        !(captures = regexp.exec(string))
+    ); // Semicolon important here, don't remove
+
+    // Ignore unmatching handlers
+    if (!captures) { return; }
+
+    const key  = keys[n];
+    const path = captures.input.slice(0, captures.index + captures[0].length);
+    const name = captures.input.slice(captures.index + captures[0].length);
+
+    // Where .base and .path have not changed, .route must have changed
+    // and params, identifier and state may have changed, so we update the 
+    // existing route and notify as route
+    if (route && route.base === base && route.path === path) {
+        route.route = name;
+        //route.params      = location.params;
+        //route.indentifier = location.identifier;
+        //route.state       = location.state;
+        return route;
+    }
+
+    // Stop old route and create a new one
+    //route && stop(route);
+    //changes.stop();
+
+    // Start of route group ***
+    if (DEBUG) { console.group('route ' + path); }
+
+    // Create a new route object
+    route = new Route(base, path, name, captures);
+
+    // Update params, identifier, state
+    route.params = location.params;
+    route.id     = location.id;
+    route.state  = location.state;
+
+    const observer = Observer(route);
+
+    // Call route handler with (route, $1, $2, ...)
+    patterns[key].apply(this, observer);
+
+    //mutations(location).each((changes) => route && assign(route, changes));
+
+    return route;
+}
+
+register('routes', function routes(patterns) {
+    const keys    = Object.keys(patterns);
+    const regexps = keys.map((pattern) => RegExp(pattern));
+
+    var route;
+
+    function routes(location) {
+        route = router(patterns, keys, regexps, location);
+        if (!route) { return; }
+        return route;
+    }
+
+    // Allow partial application:
+    // fn = routes(patterns)
+    // fn(route)
+    // or
+    // routes(patterns, route)
+
+    return arguments.length > 1 ?
+        routes(arguments[1]) :
+        routes ;
 });
-
