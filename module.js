09e274321c1e0e9b5d352c6835738133e77d0eb4
stephband <stephband@googlemail.com>
2021-07-27 15:59:12 +0200

    Module exports register()

--- module.js
+++ module.js
@@ -3,3 +3,4 @@ import './elements/literal-template.js';
 import './elements/literal-include.js';
 
 export { cache as compiled } from './modules/compile.js';
+export { register } from './modules/library.js';
