ed676739048720761b078817435daa092f2c1d03
stephband <stephband@googlemail.com>
2021-10-01 17:02:56 +0200

    Paramify

--- modules/library.js
+++ modules/library.js
@@ -167,7 +167,7 @@ const library = {
     paramify: function(object) {
         // If this is an object with properties that may be arrays, flatten it
         // out into entries
-        const params = typeof object === 'object' && typeof object.length === 'number' ?        
+        const params = typeof object === 'object' && typeof object.length !== 'number' ?        
             entries(object).flatMap((entry) => (
                 entry[1] === undefined ? nothing :
                 entry[1] && typeof entry[1] === 'object' && entry[1].map ? entry[1].map((value) => [entry[0], value]) : 
@@ -175,8 +175,7 @@ const library = {
             )) :
             object ;
 
-        console.log('PARAMIFY', object, params);
-
+        //console.log('PARAMIFY', object, params);
         return new URLSearchParams(params);
     },
 
