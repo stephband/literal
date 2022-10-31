229ec1d47c50aaec1e4fe598653fa4f263bd3759
Stephen <stephband@cruncher.ch>
2022-10-31 04:04:11 +0100

    Docs

--- modules/parse-params.js
+++ modules/parse-params.js
@@ -25,6 +25,6 @@ const parseParam = capture(/^\s*(?:([\w]+|…)|(\.\.\.))\s*(?:(,)|\))|^\s*\)/, {
 });
 
 export function parseParams(string) {
-    console.log(1, string);
+    //console.log(1, string);
     return parseParam([], string);
 }
