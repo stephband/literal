5f43fd97ad15fd5c16e80aabbc64d430edc5912c
Stephen <stephband@cruncher.ch>
2023-03-21 04:38:35 +0100

    Documentation

---
+++ documentation/module.js
@@ -0,0 +1,9 @@
+
+//import '../../bolt/attributes/data-toggleable.js';
+//import '../../bolt/classes/toggle-block.js';
+
+// Classes
+import '../../bolt/classes/device.js';
+
+// Elements
+import DetailsToggle from "../../details-toggle/module.js";
