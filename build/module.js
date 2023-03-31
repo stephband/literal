37fa12f41fb081c4e005b838bfeaefabed4a2fd6
Stephen <stephband@cruncher.ch>
2023-03-31 06:23:55 +0200

    Literal

--- build/module.js
+++ build/module.js
@@ -1,6 +1,6 @@
 /* Literal 
    0.6.4
    By Stephen Band
-   Built 2023-03-31 04:19 */
+   Built 2023-03-31 04:23 */
 
-import{g as r,h as e,l as o,m as a,p as t,s as f}from"./chunk-OJRHAA2D.js";export{r as Data,f as Renderer,a as compiled,o as library,e as observe,t as urls};
+import{g as r,h as e,l as o,m as a,p as t,s as f}from"./chunk-4WSG3XX6.js";export{r as Data,f as Renderer,a as compiled,o as library,e as observe,t as urls};
