09b3c694369ac062c49a444a74d9f5157b55e1ef
Stephen <stephband@cruncher.ch>
2023-11-21 17:11:35 +0100

    Detect writeable properties. SVG properties are not writeable after all.

--- modules/renderer/property-names.js
+++ modules/renderer/property-names.js
@@ -1,56 +1,44 @@
 
 // A map of attribute names to property names for attribute renderers. Readonly
 // properties of an element cannot be set, obviously. Marking them as null means
-// AttributeRenderer does not try to set them as properties.
+// AttributeRenderer does not try to set them as properties but as attributes.
 
 // Todo: this prevents use of custom elements that may have different
-// definitions of these property names. We should use feature detection or
-// check the attributes against their element's tag name..
+// definitions of these property names. We should perhaps use feature detection
+// or check the attributes against their element's tag name. Or we should rely
+// on custom element authors to respect existing attribute behaviour. Mmm-hmm.
 
 export default {
-    // HTML
     'accept-charset': 'acceptCharset',
-    accesskey:       'accessKey',
-    cellpadding:     'cellPadding',
-    cellspacing:     'cellSpacing',
-    codebase:        'codeBase',
-    colspan:         'colSpan',
-    datetime:        'dateTime',
-    'for':           'htmlFor',    // <label>
-    form:            null,         // Readonly
-    formaction:      'formAction', // <input>
-    formenctype:     'formEnctype',
-    formmethod:      'formMethod',
-    formnovalidate:  'formNoValidate',
-    formtarget:      'formTarget',
-    frameborder:     'frameBorder',
-    href:            null,         // <use href> is readonly
-    httpequiv:       'httpEquiv',
-    longdesc:        'longDesc',
-    maxlength:       'maxLength',
-    minlength:       'minLength',
-    nohref:          'noHref',
-    noresize:        'noResize',
-    noshade:         'noShade',    // <hr>
-    nowrap:          'noWrap',
-    novalidate:      'noValidate', // <form>
-    readonly:        'readOnly',
-    rowspan:         'rowSpan',
-    tabindex:        'tabIndex',
-    tfoot:           'tFoot',
-    thead:           'tHead',
-    usemap:          'useMap',
-    valign:          'vAlign',
-    valuetype:       'valueType',   // <object>
-
-    // SVG
-    viewbox:         null,
-    viewBox:         null,
-    x:               null,
-    y:               null,
-    dx:              null,
-    dy:              null,
-    cx:              null,
-    cy:              null,
-    r:               null
+    'accesskey':      'accessKey',
+    'cellpadding':    'cellPadding',
+    'cellspacing':    'cellSpacing',
+    'codebase':       'codeBase',
+    'colspan':        'colSpan',
+    'datetime':       'dateTime',
+    'for':            'htmlFor',    // <label>
+    'form':           null,         // Readonly
+    'formaction':     'formAction', // <input>
+    'formenctype':    'formEnctype',
+    'formmethod':     'formMethod',
+    'formnovalidate': 'formNoValidate',
+    'formtarget':     'formTarget',
+    'frameborder':    'frameBorder',
+    'httpequiv':      'httpEquiv',
+    'longdesc':       'longDesc',
+    'maxlength':      'maxLength',
+    'minlength':      'minLength',
+    'nohref':         'noHref',
+    'noresize':       'noResize',
+    'noshade':        'noShade',    // <hr>
+    'nowrap':         'noWrap',
+    'novalidate':     'noValidate', // <form>
+    'readonly':       'readOnly',
+    'rowspan':        'rowSpan',
+    'tabindex':       'tabIndex',
+    'tfoot':          'tFoot',
+    'thead':          'tHead',
+    'usemap':         'useMap',
+    'valign':         'vAlign',
+    'valuetype':      'valueType',   // <object>
 };
