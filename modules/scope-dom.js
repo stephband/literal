cde636f326c773ce835df23e6d25b157dbfc3449
Marco Bonetti <marco@cruncher.ch>
2023-11-23 17:02:56 +0100

    Import create in modules

--- modules/scope-dom.js
+++ modules/scope-dom.js
@@ -8,11 +8,13 @@ import { trigger }             from '../../dom/modules/trigger.js';
 import { px, em, rem, vw, vh } from '../../dom/modules/parse-length.js';
 import validate                from '../../dom/modules/validate.js';
 import library                 from './scope.js';
+import create                  from '../../dom/modules/create.js';
 
 
 // Export a library with DOM functions.
 
 export default Object.assign({
+    create,
     delegate,
 
     /**
