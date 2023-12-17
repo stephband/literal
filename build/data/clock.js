3f25045fc3fce1edaf8a93e050702ab98b851ad6
Stephen <stephband@cruncher.ch>
2023-12-17 16:34:52 +0100

    Style

--- build/data/clock.js
+++ build/data/clock.js
@@ -1,6 +1,6 @@
 /* Literal 
    0.8.1
    By Stephen Band
-   Built 2023-12-17 15:27 */
+   Built 2023-12-17 15:34 */
 
-import"../chunk-OYIWIDMI.js";import{m as t}from"../chunk-5BYK4RYS.js";var e=t({time:0});setInterval(()=>e.time=window.performance.now()/1e3,1e3);var r=e;export{r as default};
+import"../chunk-UGZNLPKN.js";import{m as t}from"../chunk-KLG2S3WX.js";var e=t({time:0});setInterval(()=>e.time=window.performance.now()/1e3,1e3);var r=e;export{r as default};
--- build/module.js
+++ build/module.js
@@ -1,6 +1,6 @@
 /* Literal 
    0.8.1
    By Stephen Band
-   Built 2023-12-17 15:27 */
+   Built 2023-12-17 15:34 */
 
-import"./chunk-OYIWIDMI.js";import{d as a,e as b,l as c,m as d,q as e,s as f,v as g}from"./chunk-5BYK4RYS.js";export{d as Data,g as Renderer,a as compiled,b as config,c as observe,e as scope,f as urls};
+import"./chunk-UGZNLPKN.js";import{d as a,e as b,l as c,m as d,q as e,s as f,v as g}from"./chunk-KLG2S3WX.js";export{d as Data,g as Renderer,a as compiled,b as config,c as observe,e as scope,f as urls};
