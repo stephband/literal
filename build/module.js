6ae83f5e5f64d83fa282e2c527c5b781eba85101
Stephen <stephband@cruncher.ch>
2023-12-18 00:09:31 +0100

    Code blocks

--- build/module.js
+++ build/module.js
@@ -1,6 +1,6 @@
 /* Literal 
    0.8.2
    By Stephen Band
-   Built 2023-12-17 23:05 */
+   Built 2023-12-17 23:09 */
 
-import"./chunk-EFONQPIJ.js";import{d as a,e as b,l as c,m as d,q as e,s as f,v as g}from"./chunk-I2W54ON5.js";export{d as Data,g as Renderer,a as compiled,b as config,c as observe,e as scope,f as urls};
+import"./chunk-BGV5LB56.js";import{d as a,e as b,l as c,m as d,q as e,s as f,v as g}from"./chunk-LMZ5ITUE.js";export{d as Data,g as Renderer,a as compiled,b as config,c as observe,e as scope,f as urls};
