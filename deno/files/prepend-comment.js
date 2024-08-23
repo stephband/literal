57a6088ad1f81631e3fcd30b47c90eb96f5eafb8
Marco Bonetti <marco@cruncher.ch>
2024-08-23 08:49:47 +0200

    prependComment: make path relative to cwd to avoid output changes when someone else runs make-literal

--- deno/files/prepend-comment.js
+++ deno/files/prepend-comment.js
@@ -1,6 +1,6 @@
 export default function prependComment(source, target, string) {
     return (target.endsWith('.css') || target.endsWith('.js')) ?
-        '/* Literal template "' + source + '" */\n' + string.replace(/^\s*, ''/) :
+        '/* Literal template "' + source.replace(Deno.cwd(), '') + '" */\n' + string.replace(/^\s*, ''/) :
     target.endsWith('.html') ?
         string.replace(/^\s(\<\!DOCTYPE html\>)?/, ($0, doctype) =>
             (doctype ? doctype + '\n' : '') +
