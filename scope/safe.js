d2fba5740260b62f1ae567ddacf2d23461d4f7c5
Stephen <stephband@cruncher.ch>
2024-08-30 15:36:47 +0200

    Docs

--- scope/safe.js
+++ scope/safe.js
@@ -1,6 +1,16 @@
 
 import create from 'dom/create.js';
 
+/**
+safe(html)
+Parses `html` and returns a fragment.
+
+By default Literal safely renders strings into the DOM via `node.textContent`,
+preventing injection attacks. When you need to render HTML (and your data is
+known to be safe) call `safe()` on it. Literal appends the resulting fragment
+directly.
+**/
+
 export default function safe(html) {
     return create('fragment', html);
 }
