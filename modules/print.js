2a3c6e9f8a2e852b8bced1c1a16e41549a8f36bc
Stephen <stephband@cruncher.ch>
2024-09-12 01:21:30 +0200

    Moves print CSS to JS, rmoving CSS dependency for running Literal in DEBUG mode

--- modules/print.js
+++ modules/print.js
@@ -14,13 +14,6 @@ Renders as:
 <template is="literal-html" data="../../../package.json">
     ${ print(data) }
 </template>
-
-<!--
-Messages should be styled with the print stylesheet:
-
-```css
-@import "http://stephen.band/literal/modules/print.css";
-```-->
 **/
 
 import noop    from 'fn/noop.js';
@@ -28,65 +21,149 @@ import Data    from 'fn/data.js';
 import create  from 'dom/create.js';
 import { log } from './log.js';
 
-const linkHTML = '<a class="literal-link" href="https://stephen.band/literal/literal-html/">literal</literal>';
+const assign = Object.assign;
 
-function toHTML(object) {
-    // Print different kinds of objects differently
-    if (typeof object === 'object' && object.template) {
-        return '<strong>' + object.id + '</strong> ' + object.template + ' <small>&gt; ' + object.path + '</small> <!--strong class="literal-count">' + object.count + '</strong-->';
-    }
+const literalCSS = {
+    'box-sizing':    'border-box',
+    'position':      'relative',
+    'display':       'block',
+    /* Try to not interfere with layout of surrounding content too much */
+    'overflow':      'hidden',
+    'break-inside':  'avoid',
+    'width':         '100%',
+    'justify-self':  'stretch',
+    'align-self':    'start',
+    'text-align':    'left',
+    'font-size':     '0.75rem !important',
+    'font-family':   '"Fira Mono", Menlo, Monaco, "Andale Mono", monospace',
+    'font-weight':   'normal',
+    'letter-spacing': '-0.05em',
+    'line-height':   '1.5rem !important',
+    'border-width':  '0',
+    /* Bottom left corner underlaps <code> corner, we give it a slightly bigger
+       radius to avoid aliasing this background color through the edge. */
+    'border-radius': '0 0.5rem 0 0.6875rem',
+    'padding':       '0 0.375rem'
+};
 
-    if (typeof object === 'object' && object.message) {
-        return '<code class="white-fg">' + object.message + '</code>';
-    }
+const literalLinkCSS = {
+    'box-sizing':    'border-box',
+    'position':      'absolute',
+    'bottom':        '0.25rem',
+    'right':         '0.25rem',
+    'z-index':       '2',
+    'padding-left':  '0',
+    'padding-right': '0',
+    'font-size':     '0.6875em',
+    'font-family':   '"Fira Mono", Menlo, Monaco, "Andale Mono", monospace',
+    'line-height':   '1em',
+    'opacity':       '0.86666667',
+    'color':         '#81868f',
+    'background-color': 'transparent',
+    'text-decoration': 'none'
+};
 
-    if (typeof object === 'object') {
-        return '<code><strong>' + object.constructor.name + '</strong> ' + JSON.stringify(object) + '</code>';
-    }
-}
+const literalSmallCSS = {
+    'box-sizing':   'border-box',
+    'font-size':    '0.875em',
+    'opacity':      '0.86666667'
+};
+
+const literalCodeCSS = {
+    'box-sizing':   'border-box',
+    'display':      'block',
+    'font-family':  'inherit',
+    /* Nothing special about this number */
+    'font-size':    '0.93333em',
+    'line-height':  '1.5em',
+    'white-space':  'normal',
+    'border-top':   'inherit',
+    'border-bottom': 'inherit',
+    'border-radius': '0',
+    'padding':      '0.5em 0.375rem',
+    'width':        'auto',
+    'margin-left':  '-0.375rem',
+    'margin-right': '-0.375rem',
+    'color':        '#141B1E'
+};
+
+const literalCountCSS = {
+    'box-sizing': 'border-box',
+    'position':   'absolute',
+    'top':        '0',
+    'right':      '0.375rem'
+};
 
 export function printError(renderer, error) {
     // TODO: get the data in here!
-
-    //const fullpath = renderer.path
-    //    + (typeof renderer.name === 'string' ? '>' + renderer.name : '') ;
-
     log('error', renderer.template + ' – ' + renderer.code, '', '', 'red');
     console.log(error);
 
     return create('pre', {
-        class: 'literal-error',
-        html: renderer.template
-            //+ ' <small>&gt; ' + fullpath.replace(/>/g, ' &gt ') + '</small>'
-            //+ '&nbsp;&nbsp;'
-            + ' <small class="literal-message">' + renderer.code.replace(/</g, '&lt;').replace(/>/g, '&gt;') + '</small>'
-            + '<code>'
-            + '<strong>' + error.constructor.name + '</strong> '
-            + error.message.replace(/</g, '&lt;').replace(/>/g, '&gt;')
-            + '</code>'
-            + linkHTML
+        children: [
+            renderer.template + ' ',
+
+            create('small', {
+                text:  renderer.code/*.replace(/</g, '&lt;').replace(/>/g, '&gt;')*/,
+                style: literalSmallCSS
+            }),
+
+            create('code', {
+                html: '<strong>' + error.constructor.name + '</strong> ' + error.message.replace(/</g, '&lt;').replace(/>/g, '&gt;'),
+                style: assign({ 'background-color': '#ffdddd' }, literalCodeCSS)
+            }),
+
+            create('a', {
+                text: 'literal',
+                href: 'https://stephen.band/literal/literal-html/',
+                style: literalLinkCSS
+            })
+        ],
+
+        style: assign({ 'color': 'white', 'background-color': '#DC0F0E' }, literalCSS)
     });
 }
 
-export function printDebug(renderer, object) {
+export function printDebug(renderer, error) {
     const fullpath = renderer.path
         + (typeof renderer.name === 'string' ? '>' + renderer.name : '') ;
 
+    const children = [
+        renderer.template + ' ',
+
+        create('small', {
+            text:  (fullpath ? '> ' + fullpath.replace(/>/g, ' > ') : ''),
+            style: literalSmallCSS
+        }),
+
+        create('span', {
+            text: renderer.renderCount,
+            style: literalCountCSS
+        })
+    ];
+
     let n = 0;
-    let html = '';
-    while (arguments[++n] !== undefined) {
-        html += toHTML(arguments[n]);
+    let object;
+    while ((object = arguments[++n]) !== undefined) {
+        children.push(create('code', {
+            children: [
+                create('strong', { text: object.constructor.name }),
+                JSON.stringify(object)
+            ],
+
+            style: assign({ 'background-color': '#c5dded' }, literalCodeCSS)
+        }));
     }
 
+    children.push(create('a', {
+        text: 'literal',
+        href: 'https://stephen.band/literal/literal-html/',
+        style: literalLinkCSS
+    }));
+
     return create('pre', {
-        class: 'literal-print',
-        html: renderer.template
-            + ' <small>' + (fullpath ? '&gt; ' + fullpath.replace(/>/g, ' &gt ') : '') + '</small>'
-            //+ '&nbsp;&nbsp;'
-            //+ ' <small class="literal-message">' + renderer.code.replace(/</g, '&lt;').replace(/>/g, '&gt;') + '</small>'
-            + '<span class="literal-count">' + renderer.renderCount + '</span>'
-            + html
-            + linkHTML
+        children: children,
+        style: assign({ 'color': 'white', 'background-color': '#46789a' }, literalCSS)
     });
 }
 
